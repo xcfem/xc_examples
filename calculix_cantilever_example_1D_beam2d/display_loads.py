b4459c672b3d5661638167aacff7951445115c0e
Luis C. Pérez Tato <l.pereztato@gmail.com>
2019-09-18 17:30:06 +0200

    Added example using beam 2D elements.
    I've added this example to detect problems in methods
    that display axis etc when working in 2D. Work in progress.

---
+++ calculix_cantilever_example_1D_beam2d/display_loads.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from postprocess.reports import graphical_reports as gr
+from postprocess.xcVtk import vtk_graphic_base
+from postprocess.xcVtk.FE_model import quick_graphics as qg
+
+execfile('./cantilever_mesh_generation.py')
+
+#available components: 'axialComponent', 'transComponent', 'transYComponent',
+#                      'transZComponent'
+
+rlcd= gr.getRecordLoadCaseDispFromLoadPattern(lp0)
+rlcd.cameraParameters= vtk_graphic_base.CameraParameters('Custom')
+rlcd.cameraParameters.viewUpVc= [0,1,0]
+rlcd.cameraParameters.posCVc= [-100,100,100]
+rlcd.setsToDispLoads=[xcTotalSet]
+rlcd.setsToDispBeamLoads=[xcTotalSet]
+
+loadCasesToDisplay=[rlcd]
+
+#End data
+
+for lc in loadCasesToDisplay:
+    lc.displayLoadOnSets()
