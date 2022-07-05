13b2652fd3e6d26627eb7a745d50a8c756698ace
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-07-05 15:49:39 +0200

    Simplify script

--- reinforced_concrete/anchorage_and_lap_length/ehe_anchorage_overlap_lengths.py
+++ reinforced_concrete/anchorage_and_lap_length/ehe_anchorage_overlap_lengths.py
@@ -5,9 +5,10 @@
 from __future__ import division
 from __future__ import print_function
 
+import math
+import os
 from materials.ehe import EHE_materials
 from materials.ehe import EHE_limit_state_checking
-import os
 from misc_utils import log_messages as lmsg
 from collections import OrderedDict
 from pyexcel_ods import save_data
@@ -19,17 +20,22 @@ __version__= "3.0"
 __email__= "l.pereztato@gmail.com"
 
 # Materials
-concreteList= [EHE_materials.HA30, EHE_materials.HP50]
+concreteList= [EHE_materials.HA30]# , EHE_materials.HP50]
 steel= EHE_materials.B500S
 diameters= [8e-3, 10e-3, 12e-3, 16e-3, 20e-3, 25e-3]
-spacing= [0.1,0.2]
-cCover= .025
+spacing= [0.10] #[0.1,0.2]
+cCover= .035
 
 # Rebar controller.
-rebarControllerPosI= EHE_limit_state_checking.RebarController(concreteCover= cCover, pos= 'I', compression= False)
-rebarControllerPosII= EHE_limit_state_checking.RebarController(concreteCover= cCover, pos= 'II', compression= False)
+rebarControllerPosI= EHE_limit_state_checking.RebarController(concreteCover= cCover, pos= 'I', compression= True)
+rebarControllerPosII= EHE_limit_state_checking.RebarController(concreteCover= cCover, pos= 'II', compression= True)
 
 rows= list([[u'Hormigón', u'Diámetro', u'Separación', 'LsI', 'LsII', 'LnI', 'LnII'], ['','(mm)', '(cm)', '(cm)', '(cm)', '(cm)', '(cm)']])
+
+fmt= '{:.0f}'
+
+def to5cm(value):
+    return (math.ceil(value/.05)*.05)*100
 
 #  length.
 for c in concreteList:
@@ -40,7 +46,7 @@ for c in concreteList:
             lbI= rebarControllerPosI.getNetAnchorageLength(c, diam, steel, steelEfficiency= 1.0, barShape= 'straight')
             lbII= rebarControllerPosII.getNetAnchorageLength(c, diam, steel, steelEfficiency= 1.0, barShape= 'straight')
             prevRow= rows[-1]
-            row= [str(c), diam*1e3, s*100, lsI*100.0, lsII*100.0, lbI*100.0, lbII*100.0]
+            row= [str(c), diam*1e3, fmt.format(s*100), to5cm(lsI), to5cm(lsII), to5cm(lbI), to5cm(lbII)]
             if((row[3]!= prevRow[3]) or (row[4]!= prevRow[4]) or (row[5]!= prevRow[5]) or (row[6]!= prevRow[6])):
                 if(row[1]==prevRow[1]):
                     row[1]= ''
