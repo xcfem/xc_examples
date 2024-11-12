f15c754cc93a98e6df400297cc8cc6d84f1ea8d6
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-11-12 18:31:51 +0100

    Code cosmetics

--- geotechnics/sheet_pile_wall/nonlinear_soil_spring_model_test_01.py
+++ geotechnics/sheet_pile_wall/nonlinear_soil_spring_model_test_01.py
@@ -28,11 +28,12 @@ modelSpace= predefined_spaces.StructuralMechanics2D(nodes)
 # Materials definition
 ## Soil material
 soil= earth_pressure.RankineSoil(phi= math.radians(32), rho= 15.90e3/g, rhoSat= 19.33e3/g)
+soil.Kh= 30e6
+
 ## Pile material.
 concr= EHE_materials.HA30
 steel= EHE_materials.B500S
 diameter= 450e-3 # Cross-section diameter [m]
-cover= 0.07 # Cover [m]
 pileSection= def_column_RC_section.RCCircularSection(name='test',Rext= diameter/2.0, concrType=concr, reinfSteelType= steel)
 xcPileSection= pileSection.defElasticShearSection2d(preprocessor)
 
@@ -41,11 +42,9 @@ pileSpacing= 1.0 # One pile every meter.
 L1= 5.0 #5.0 # Excavation depth (m)
 Dteory= 4.7
 L= L1+1.3*Dteory # Total lenght (m)
-points= preprocessor.getMultiBlockTopology.getPoints
-pt1= points.newPoint(geom.Pos3d(0,0,0))
-pt2= points.newPoint(geom.Pos3d(0,-L,0))
-lines= preprocessor.getMultiBlockTopology.getLines
-ln= lines.newLine(pt1.tag, pt2.tag)
+pt1= modelSpace.newKPoint(0,0,0)
+pt2= modelSpace.newKPoint(0,-L,0)
+ln= modelSpace.newLine(pt1, pt2)
 ln.setElemSize(0.25)
 
 # Mesh generation
@@ -64,7 +63,6 @@ ln.genMesh(xc.meshDir.I)
 modelSpace.fixNodeF0F(pt2.getNode().tag) # Fix vertical displacement.
 
 ## Define nonlinear springs.
-Kh= 30e6
 
 ### Define soil response diagrams.
 soilResponseMaterials= dict()
@@ -82,9 +80,8 @@ for n in ln.nodes:
         tributaryLength= n.getTributaryLength()
         tributaryArea= tributaryLength*pileSpacing
         materialName= 'soilResponse_z_'+str(n.tag)
-        nonLinearSpringMaterial= soil.defHorizontalSubgradeReactionNlMaterial(preprocessor, name= materialName, depth= nodeDepth, tributaryArea= tributaryArea, Kh= Kh)
+        nonLinearSpringMaterial= soil.defHorizontalSubgradeReactionNlMaterial(preprocessor, name= materialName, depth= nodeDepth, tributaryArea= tributaryArea, Kh= soil.Kh)
         
-        #print('node: ', n.tag, ' node depth: ', '{:.2f}'.format(nodeDepth), ' tributary area: ',  '{:.2f}'.format(tributaryArea), 'init strain: ', initStrain)
     tributaryAreas[n.tag]= tributaryArea
     soilResponseMaterials[n.tag]= nonLinearSpringMaterial
 
@@ -122,9 +119,9 @@ for i, pair in enumerate(springPairs):
         zlRight.setupVectors(xc.Vector([1,0,0]),xc.Vector([0,1,0]))
         rightZLElements[nodeTag]= zlRight
 
-numSteps= 10
 
 # Solve 
+numSteps= 10
 #solProc= predefined_solutions.PenaltyKrylovNewton(prb= feProblem, numSteps= numSteps, maxNumIter= 300, convergenceTestTol= 1e-6, printFlag= 0)
 solProc= predefined_solutions.PenaltyNewtonRaphson(prb= feProblem, numSteps= numSteps, maxNumIter= 300, convergenceTestTol= 1e-5, printFlag= 0)
 ok= solProc.solve()
@@ -133,7 +130,7 @@ if(ok!=0):
     exit(1)
 
 reactionCheckTolerance= 1e-6
-updatedElements= ep.excavation_process(preprocessor= preprocessor, solProc= solProc, nodesToExcavate= nodesToExcavate, elementsOnExcavationSide= leftZLElements, maxDepth= 5.0, tributaryAreas= tributaryAreas, soil= soil, Kh= Kh)
+updatedElements= ep.excavation_process(preprocessor= preprocessor, solProc= solProc, nodesToExcavate= nodesToExcavate, elementsOnExcavationSide= leftZLElements, maxDepth= 5.0, tributaryAreas= tributaryAreas, soil= soil)
 modelSpace.calculateNodalReactions(reactionCheckTolerance= reactionCheckTolerance)
 
 finalResults= ep.get_results_dict(soil= soil, tributaryAreas= tributaryAreas, springPairs= springPairs, pileWallElements= [ln])
