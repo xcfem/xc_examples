c58fdc574516fe6578d74987ca6437a339d41ee0
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-11-14 10:07:07 +0100

    Debugging code
    Renaming tests and searching for the bug: it is related with using more
    than one line to create the model.

--- geotechnics/sheet_pile_wall/nonlinear_soil_spring_model_test_01.py
+++ geotechnics/sheet_pile_wall/nonlinear_soil_spring_model_test_01.py
@@ -6,9 +6,6 @@ import geom
 import xc
 from scipy.constants import g
 from geotechnics import earth_pressure
-from materials import typical_materials
-from model import predefined_spaces
-from solution import predefined_solutions
 from materials.sections.fiber_section import def_column_RC_section
 from materials.ehe import EHE_materials
 from materials.ehe import EHE_limit_state_checking
@@ -18,129 +15,42 @@ from tabulate import tabulate
 
 lmsg.log('WARNING: work in progress. Use with caution.')
 
-# Define finite element problem.
-feProblem= xc.FEProblem()
-preprocessor=  feProblem.getPreprocessor   
-nodes= preprocessor.getNodeHandler
-## Problem type
-modelSpace= predefined_spaces.StructuralMechanics2D(nodes)
-
 # Materials definition
 ## Soil material
 soil= earth_pressure.RankineSoil(phi= math.radians(32), rho= 15.90e3/g, rhoSat= 19.33e3/g)
 soil.Kh= 30e6
+### Soil strata.
+L1= 5.0 #5.0 # Excavation depth (m)
+Dteory= 4.7
+L= L1+1.3*Dteory # Total lenght (m)
+soilLayersDepths= [0.0, L]
+soilLayers= [soil, soil]
 
 ## Pile material.
 concr= EHE_materials.HA30
 steel= EHE_materials.B500S
 diameter= 450e-3 # Cross-section diameter [m]
 pileSection= def_column_RC_section.RCCircularSection(name='test',Rext= diameter/2.0, concrType=concr, reinfSteelType= steel)
-xcPileSection= pileSection.defElasticShearSection2d(preprocessor)
 
-# Problem geometry
-pileSpacing= 1.0 # One pile every meter.
-L1= 5.0 #5.0 # Excavation depth (m)
-Dteory= 4.7
-L= L1+1.3*Dteory # Total lenght (m)
-pt1= modelSpace.newKPoint(0,0,0)
-pt2= modelSpace.newKPoint(0,-L,0)
-ln= modelSpace.newLine(pt1, pt2)
-ln.setElemSize(0.25)
+pileWall= ep.PileWall(pileSection= pileSection, soilLayersDepths= soilLayersDepths, soilLayers= soilLayers, excavationDepth= L1, pileSpacing= 1.0, waterTableDepth= None)
 
 # Mesh generation
-## Geometric transformations
-lin= modelSpace.newLinearCrdTransf("lin")
-## Seed element
-seedElemHandler= preprocessor.getElementHandler.seedElemHandler
-seedElemHandler.dimElem= 2 # Bars defined in a two-dimensional space.
-seedElemHandler.defaultMaterial= xcPileSection.name
-seedElemHandler.defaultTransformation= lin.name
-beam2d= seedElemHandler.newElement("ElasticBeam2d")
-beam2d.h= diameter
-ln.genMesh(xc.meshDir.I)
-
-## Constraints.
-modelSpace.fixNodeF0F(pt2.getNode().tag) # Fix vertical displacement.
-
-## Define nonlinear springs.
-
-### Define soil response diagrams.
-soilResponseMaterials= dict()
-tributaryAreas= dict()
-nodesToExcavate= list() # Nodes in the excavation depth.
-ln.resetTributaries()
-ln.computeTributaryLengths(False) # Compute tributary lenghts.
-for n in ln.nodes:
-    nodeDepth= -n.getInitialPos3d.y
-    if(nodeDepth<L1):
-        nodesToExcavate.append((nodeDepth, n))
-    nonLinearSpringMaterial= None
-    tributaryArea= 0.0
-    if(nodeDepth>0.0): # Avoid zero soil response.
-        tributaryLength= n.getTributaryLength()
-        tributaryArea= tributaryLength*pileSpacing
-        materialName= 'soilResponse_z_'+str(n.tag)
-        nonLinearSpringMaterial= soil.defHorizontalSubgradeReactionNlMaterial(preprocessor, name= materialName, depth= nodeDepth, tributaryArea= tributaryArea, Kh= soil.Kh)
-        
-    tributaryAreas[n.tag]= tributaryArea
-    soilResponseMaterials[n.tag]= nonLinearSpringMaterial
-
-pileSet= preprocessor.getSets.defSet('pileSet')
-for e in ln.elements:
-    pileSet.getElements.append(e)
-
-# Sort nodes to excavate on depth.
-
-### Duplicate nodes below ground level.
-springPairs= list()
-for n in ln.nodes:
-    newNode= nodes.duplicateNode(n.tag)
-    modelSpace.fixNode000(newNode.tag)
-    springPairs.append((n, newNode))
-
-### Define Spring Elements
-elements= preprocessor.getElementHandler
-elements.dimElem= 2 #Element dimension.
-leftZLElements= dict()
-rightZLElements= dict()
-for i, pair in enumerate(springPairs):
-    nodeTag= pair[0].tag
-    soilResponseMaterial= soilResponseMaterials[nodeTag]
-    if(soilResponseMaterial): # Spring defined for this node.
-        # Material for the left spring
-        elements.defaultMaterial= soilResponseMaterial.name
-        # Springs on the left side of the beam
-        zlLeft= elements.newElement("ZeroLength",xc.ID([pair[1].tag, pair[0].tag]))
-        zlLeft.setupVectors(xc.Vector([-1,0,0]),xc.Vector([0,-1,0]))
-        leftZLElements[nodeTag]= zlLeft
-        
-        # Springs on the right side of the beam
-        zlRight= elements.newElement("ZeroLength",xc.ID([pair[0].tag, pair[1].tag]))
-        zlRight.setupVectors(xc.Vector([1,0,0]),xc.Vector([0,1,0]))
-        rightZLElements[nodeTag]= zlRight
+pileWall.genMesh()
 
 
 # Solve 
-numSteps= 10
-#solProc= predefined_solutions.PenaltyKrylovNewton(prb= feProblem, numSteps= numSteps, maxNumIter= 300, convergenceTestTol= 1e-6, printFlag= 0)
-solProc= predefined_solutions.PenaltyNewtonRaphson(prb= feProblem, numSteps= numSteps, maxNumIter= 300, convergenceTestTol= 1e-5, printFlag= 0)
-ok= solProc.solve()
-if(ok!=0):
-    lmsg.error('Can\'t solve')
-    exit(1)
+pileWall.solve(excavationSide= 'left')
 
-reactionCheckTolerance= 1e-6
-updatedElements= ep.excavation_process(preprocessor= preprocessor, solProc= solProc, nodesToExcavate= nodesToExcavate, elementsOnExcavationSide= leftZLElements, maxDepth= 5.0, tributaryAreas= tributaryAreas, soil= soil)
-modelSpace.calculateNodalReactions(reactionCheckTolerance= reactionCheckTolerance)
+# Get results.
+results= pileWall.getResultsDict()
 
-finalResults= ep.get_results_dict(soil= soil, tributaryAreas= tributaryAreas, springPairs= springPairs, pileWallElements= [ln])
-outputTable= ep.get_results_table(resultsDict= finalResults)
+outputTable= ep.get_results_table(resultsDict= results)
 
 # Compute maximum bending moment.
 MMin= 6.023e23
 MMax= -MMin
-for nodeTag in finalResults:
-    nodeResults= finalResults[nodeTag]
+for nodeTag in results:
+    nodeResults= results[nodeTag]
     depth= nodeResults['depth']
     M= nodeResults['M']
     MMin= min(MMin, M)
@@ -170,7 +80,7 @@ else:
     lmsg.error('test: '+fname+' ERROR.')
 
 # Matplotlib output.
-# ep.plot_results(resultsDict= finalResults, title= 'Based on the example 14.2 of the book "Principles of Foundation Engineering" of Braja M. Das.')
+# ep.plot_results(resultsDict= results, title= 'Based on the example 14.2 of the book "Principles of Foundation Engineering" of Braja M. Das.')
 
 # # VTK Graphic output.
 # from postprocess import output_handler
