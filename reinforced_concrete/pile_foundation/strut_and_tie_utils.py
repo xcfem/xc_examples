b094bd49d2ed143969b91221687f6f85ad8821e4
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-02-23 16:11:38 +0100

    Create strut-and-tie model for triangular pilecap (work in progress)

--- reinforced_concrete/pile_foundation/strut_and_tie_utils.py
+++ reinforced_concrete/pile_foundation/strut_and_tie_utils.py
@@ -13,6 +13,7 @@ __email__= "l.pereztato@gmail.com"
 from model.mesh import strut_and_tie_utils
 import uuid
 import geom
+import xc
 
 class Pilecap3Piles(object):
     def __init__(self, pierBottomNode, pileTopNodeA, pileTopNodeB, pileTopNodeC, pierEffectiveDiameter):
@@ -31,14 +32,21 @@ class Pilecap3Piles(object):
         self.pierEffectiveDiameter= pierEffectiveDiameter
         
 
-    def createStrutAndTieModel(self, modelSpace, strutArea, concrete, pileTiesArea, topChordsTiesArea, bottomChordsTiesArea, reinfSteel, xcPierSectionMaterial, linearElastic= False):
+    def createStrutAndTieModel(self, modelSpace, strutArea, concrete, pileTiesArea, radialTiesArea, bottomChordsTiesArea, topChordsTiesArea, shearTiesArea, reinfSteel, xcPierSectionMaterial, linearElastic= False):
         ''' Creates an strut-and-tie model and attach it to the nodes of the 
             pier and the piles.
 
         :param modelSpace: PredefinedSpace object used to create the FE model.
         :param strutAre: area of the struts.
         :param concrete: concrete material.
-        :param topDownTiesArea: area of the ties.
+        :param pileTiesArea: vertical ties over each pile.
+        :param radialTiesArea= area of the radial ties joining the pier and 
+                               the piles.
+        :param bottomChordTiesArea= area of the ties joining the heads of the
+                                    piles. 
+        :param topChordTiesArea= area of the ties joining the tips of the 
+                                 vertical ties on each pile.
+        :param shearTiesArea: area of the vertical ties between piles.
         :param reinfSteel: reinforcing steel material.
         :param xcPierSectionMaterial: XC section material for the pier.
         :param linearElastic: if True, assign a linear elastic material to
@@ -109,6 +117,7 @@ class Pilecap3Piles(object):
             concreteNoTension= concrete.defElasticNoTensMaterial(preprocessor= modelSpace.preprocessor, a= .001)
         modelSpace.setElementDimension(3)
         modelSpace.setDefaultMaterial(concreteNoTension)
+        
         ### Radial struts.
         radialStrutsNodes= [(pierTopNodeA, pileBottomNodeA), (pierBottomNodeA, pileTopNodeA), (pierTopNodeB, pileBottomNodeB), (pierBottomNodeB, pileTopNodeB), (pierTopNodeC, pileBottomNodeC), (pierBottomNodeC, pileTopNodeC)]
         self.radialStruts= list()
@@ -117,19 +126,32 @@ class Pilecap3Piles(object):
             newStrut.sectionArea= strutArea
             self.radialStruts.append(newStrut)
 
-        ### Tangential struts.
-        pilesContourBottomNodes= [pileBottomNodeA, midBottomNodeAB, pileBottomNodeB, midBottomNodeBC, pileBottomNodeC, midBottomNodeCA, pileBottomNodeA] 
-        pilesContourTopNodes= [pileTopNodeA, midTopNodeAB, pileTopNodeB, midTopNodeBC, pileTopNodeC, midTopNodeCA, pileTopNodeA]
-        self.tangentialStruts= list()
-        n0Bottom= pilesContourBottomNodes[0]
-        n0Top= pilesContourTopNodes[0]
-        for (n1Bottom, n1Top) in zip(pilesContourBottomNodes[1:], pilesContourTopNodes[1:]):
+        ### Struts on the perimeter.
+        pilesContourBottomNodes= [pileBottomNodeA, pileBottomNodeB, pileBottomNodeC] 
+        pilesContourTopNodes= [pileTopNodeA, pileTopNodeB, pileTopNodeC]
+        midContourBottomNodes= [midBottomNodeAB, midBottomNodeBC, midBottomNodeCA] 
+        midContourTopNodes= [midTopNodeAB, midTopNodeBC, midTopNodeCA]
+        contourBottomNodes= list()
+        for pileBottomNode, midBottomNode in zip(pilesContourBottomNodes, midContourBottomNodes):
+            contourBottomNodes.append(pileBottomNode)
+            contourBottomNodes.append(midBottomNode)
+        contourBottomNodes.append(contourBottomNodes[0]) # close "circuit".
+        contourTopNodes= list()
+        for pileTopNode, midTopNode in zip(pilesContourTopNodes, midContourTopNodes):
+            contourTopNodes.append(pileTopNode)
+            contourTopNodes.append(midTopNode)
+        contourTopNodes.append(contourTopNodes[0]) # close "circuit".
+        
+        self.perimeterStruts= list()
+        n0Bottom= contourBottomNodes[0]
+        n0Top= contourTopNodes[0]
+        for (n1Bottom, n1Top) in zip(contourBottomNodes[1:], contourTopNodes[1:]):
             newStrut= modelSpace.newElement("Truss", [n0Bottom.tag, n1Top.tag])
             newStrut.sectionArea= strutArea
-            self.tangentialStruts.append(newStrut)
+            self.perimeterStruts.append(newStrut)
             newStrut= modelSpace.newElement("Truss", [n0Top.tag, n1Bottom.tag])
             newStrut.sectionArea= strutArea
-            self.tangentialStruts.append(newStrut)
+            self.perimeterStruts.append(newStrut)
             n0Bottom= n1Bottom
             n0Top= n1Top
 
@@ -148,14 +170,88 @@ class Pilecap3Piles(object):
             self.pierStruts.append(newStrut)
             n0Bottom= n1Bottom
             n0Top= n1Top
-        print('XXXX continue here.')
         
+        ## Define ties.
+        ### Define material.
+        if(linearElastic):
+            steelNoCompression= reinfSteel.defElasticMaterial(preprocessor= modelSpace.preprocessor)
+        else:
+            steelNoCompression= reinfSteel.defElasticNoCompressionMaterial(preprocessor= modelSpace.preprocessor, a= .001)
+        modelSpace.setDefaultMaterial(steelNoCompression)
+        modelSpace.setElementDimension(3)
+
+        ### Define radial ties.
+        radialTiesNodes= [(pierTopNodeA, pileTopNodeA), (pierBottomNodeA, pileBottomNodeA), (pierTopNodeB, pileTopNodeB), (pierBottomNodeB, pileBottomNodeB), (pierTopNodeC, pileTopNodeC), (pierBottomNodeC, pileBottomNodeC)]
+        self.radialTies= list()
+        for (n1, n2) in radialTiesNodes:
+            newTie= modelSpace.newElement("Truss", [n1.tag, n2.tag])
+            newTie.sectionArea= radialTiesArea
+            self.radialTies.append(newTie)
+
+        ### Define ties on the perimeter.
+        #### Top chord.
+        self.topChordTies= list()
+        n0Top= contourTopNodes[0]
+        for n1Top in contourTopNodes[1:]:
+            newTie= modelSpace.newElement("Truss", [n0Top.tag, n1Top.tag])
+            newTie.sectionArea= topChordsTiesArea
+            self.topChordTies.append(newTie)
+            n0Top= n1Top
+        self.bottomChordTies= list()
+        #### Bottom chord.
+        n0Bottom= contourBottomNodes[0]
+        for n1Bottom in contourBottomNodes[1:]:
+            newTie= modelSpace.newElement("Truss", [n0Bottom.tag, n1Bottom.tag])
+            newTie.sectionArea= bottomChordsTiesArea
+            self.bottomChordTies.append(newTie)
+            n0Bottom= n1Bottom
+        #### Vertical ties over each pile.
+        for (nBottom, nTop) in zip(pilesContourBottomNodes, pilesContourTopNodes):
+            newTie= modelSpace.newElement("Truss", [nBottom.tag, nTop.tag])
+            newTie.sectionArea= pileTiesArea
+            self.bottomChordTies.append(newTie)
+        #### Shear ties between piles.
+        for (nBottom, nTop) in zip(midContourBottomNodes, midContourTopNodes):
+            newTie= modelSpace.newElement("Truss", [nBottom.tag, nTop.tag])
+            newTie.sectionArea= shearTiesArea
+            self.bottomChordTies.append(newTie)
+
+        ### Define pier ties.
+        #### Top chord.
+        self.pierTopChordTies= list()
+        n0Top= pierContourTopNodes[0]
+        for n1Top in pierContourTopNodes[1:]:
+            newTie= modelSpace.newElement("Truss", [n0Top.tag, n1Top.tag])
+            newTie.sectionArea= topChordsTiesArea
+            self.pierTopChordTies.append(newTie)
+            n0Top= n1Top
+        #### Bottom chord.
+        self.pierBottomChordTies= list()
+        n0Bottom= pierContourBottomNodes[0]
+        for n1Bottom in pierContourBottomNodes[1:]:
+            newTie= modelSpace.newElement("Truss", [n0Bottom.tag, n1Bottom.tag])
+            newTie.sectionArea= bottomChordsTiesArea
+            self.pierBottomChordTies.append(newTie)
+            n0Bottom= n1Bottom
         
+        ### Define dummy springs (to fix rotational DOFs only).
+        nodesToConstrain= contourTopNodes+midContourBottomNodes+pierContourBottomNodes
+        springsAndNodes= strut_and_tie_utils.define_dummy_springs(modelSpace, nodesToConstrain)
         
-        print('d= ', d)
-        print(pierBottomNodePosA, pierBottomNodePosA.dist(pileBottomNodePosA))
-        print(pierBottomNodePosB, pierBottomNodePosB.dist(pileBottomNodePosB))
-        print(pierBottomNodePosC, pierBottomNodePosC.dist(pileBottomNodePosC))
+        ### Connect the pier with the pilecap using the pier section
+        ### as "rigid beam".
+        modelSpace.setDefaultMaterial(xcPierSectionMaterial)
+        n0Tag= self.pierBottomNode.tag
+        n1Tag= pierContourTopNodes[1].tag
+        n2Tag= pierContourTopNodes[2].tag
+        n3Tag= pierContourTopNodes[3].tag
+        crdTransfName= str(uuid.uuid4().hex)
+        xzVector= xc.Vector([kVector.x, kVector.y, kVector.z])
+        crdTransf= modelSpace.newLinearCrdTransf(crdTransfName, xzVector= xzVector)
+        modelSpace.setDefaultCoordTransf(crdTransf)
+        modelSpace.newElement('ElasticBeam3d', [n0Tag, n1Tag])
+        modelSpace.newElement('ElasticBeam3d', [n0Tag, n2Tag])
+        modelSpace.newElement('ElasticBeam3d', [n0Tag, n3Tag])
 
     
 
